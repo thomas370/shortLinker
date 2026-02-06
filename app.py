fefd5919384c8a4a21bbec38285973208378e5ec
Thomas Bortolato <61499805+thomas370@users.noreply.github.com>
2026-02-06 14:02:22 +0100

    Add user loader for login manager

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from utils.db import db
 from auth.routes import auth_bp
 from links.routes import links_bp
 from config import Config
+from auth.models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 login_manager.login_view = "auth.login"
 
 app.register_blueprint(auth_bp)
